fee2e936b4d8609ae8de150760206a8ff4555fc2
=danaisboss <yjk9313@gmail.com>
2021-03-20 10:36:25 +0900

    schema 생성 및 분리

--- node_shop/index.js
+++ node_shop/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const connect = require('./schemas');
 const port = 3000
 const goodsRouter = require('./routes/goods');
 const userRouter = require('./routes/user');
 
+connect();
 
 app.use(express.static('public'));
 
--- node_shop/schemas/index.js
+++ node_shop/schemas/index.js
@@ -14,3 +14,5 @@ const connect = () => {
 mongoose.connection.on("error", err => {
   console.error("몽고디비 연결 에러", err);
 });
+
+module.exports = connect;
