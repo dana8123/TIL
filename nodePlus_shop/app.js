23ba617c3bf55c6645cd827ab38c6fbea50146ce
=danaisboss <yjk9313@gmail.com>
2021-03-26 20:25:19 +0900

    회원가입기능 구현

--- nodePlus_shop/app.js
+++ nodePlus_shop/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const User = require('./models/user');
 
 mongoose.connect("mongodb://localhost/shopping-demo", {
   useNewUrlParser: true,
@@ -10,6 +11,32 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 const app = express();
 const router = express.Router();
+
+router.post("/users", async(req,res)=>{
+  const { nickname, email, password, confirmPassword } = req.body;
+
+  if(password != confirmPassword){
+    res.stastus(400).send({
+      errorMessage: '패스워드가 동일하지않습니다.'
+    });
+    return; //아래의 코드를 실행할 필요가 없다
+  }
+
+  const existUser = await User.find({
+    $or: [{ email }, { nickname }],
+  });
+  if (existUser.length){
+    res.status(400).send({
+      errorMessage: '이미 가입된 이메일 또는 닉네임이 있습니다.'
+    });
+    return;
+  }
+
+  const user = new User({ email, nickname, password});
+  await user.save();
+
+  res.status(201).send({});
+});
 
 app.use("/api", express.urlencoded({ extended: false }), router);
 app.use(express.static("assets"));
