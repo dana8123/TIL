b6eb0db7b82279140d26cd64d944cd0c949cbd5b
=danaisboss <yjk9313@gmail.com>
2021-04-10 10:58:29 +0900

    Types in Typescript

---
+++ typescript/src/index.ts
@@ -0,0 +1,11 @@
+const name = "dana",
+  age = 24,
+  gender = "female";
+
+const sayHi = (name:String, age:number, gender:String) => {
+  console.log(`Hello ${name}, you ar ${age}, and ${gender}`);
+};
+
+sayHi("dana", 28, "female");
+
+export {};
