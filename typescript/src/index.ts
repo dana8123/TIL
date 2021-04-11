59520f5e3119c4c2d8e5f641c3b08d1d815dffaa
=danaisboss <yjk9313@gmail.com>
2021-04-11 23:05:09 +0900

    classes on typescript1

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,15 +1,18 @@
-interface Human {
-  name: String,
-  age: number,
-  gender: String
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  //method 클래스가 시작할 때 마다 호출됨, 클래스로부터 객체를 만들 때 마다.
+  constructor(name: string, age: number, gender: string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+//react, js등에서 클래스를 사용할 수 있음
+//ts에서는 그냥 interface쓰는게 낫다?
 
-const person = {
-  name: "dana",
-  age: 22,
-  gender: "female"
-}
-
+const lynn = new Human("Lynn", 18, "female");
 const sayHi = (person: Human) => {
   return `Hello ${person.name}, you ar ${person.age}, and ${person.gender}`;
 };
