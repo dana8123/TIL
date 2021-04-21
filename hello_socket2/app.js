c369d8760098eb303ef5d0cedbdc3a217ae68175
=danaisboss <yjk9313@gmail.com>
2021-04-21 15:39:00 +0900

    hello socket

--- hello_socket2/app.js
+++ hello_socket2/app.js
@@ -3,20 +3,19 @@ const path = require("path");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const flash = require("connect-flash");
-require("dotenv").config();
+const dotenv = require("dotenv");
 
+dotenv.config();
 const webSocket = require("./socket");
 const indexRouter = require("./routes");
 
 const app = express();
-
-app.set("views", path.join(__dirname, "vies"));
-app.set("view engine", "pug");
 app.set("port", process.env.PORT || 8005);
+app.set("view engine", "pug");
 
 app.use(morgan("dev"));
-app.use(express.static(paht.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
@@ -30,24 +29,24 @@ app.use(
 		},
 	})
 );
-app.use(flash());
+
 app.use("/", indexRouter);
 
-app.use((req, res, next) => {
-	const err = new Error("Not Found");
-	err.status = 404;
-	next(error);
-});
+// app.use((req, res, next) => {
+// 	const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+// 	error.status = 404;
+// 	next(error);
+// });
 
-app.use((err, req, res, next) => {
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
-	res.status(err.status || 500);
-	res.render("error");
-});
+// app.use((err, req, res, next) => {
+// 	res.locals.message = err.message;
+// 	res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+// 	res.status(err.status || 500);
+// 	res.send("error");
+// });
 
-app.listen(app.get("port"), () => {
-	console.log(app.get("port", "번 포트에서 대기중"));
+const server = app.listen(app.get("port"), () => {
+	console.log(app.get("port"), "번 포트에서 대기중");
 });
 
 webSocket(server);
--- hello_socket2/socket.js
+++ hello_socket2/socket.js
@@ -1,23 +1,32 @@
-const webSocket = require("ws");
+const WebSocket = require("ws");
 
 module.exports = (server) => {
-	const wss = new WebSocket.server({ server });
+	const wss = new WebSocket.Server({ server });
 
 	wss.on("connection", (ws, req) => {
+		//클라이언트의 ip를 알아내는 코드
+		//proxy addr패키지를 사용해도 좋다고 함
 		const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
 		console.log("새로운 클라이언트 접속", ip);
+		//클라이언트로부터 메세지를 받았을 때 발생
 		ws.on("message", (message) => {
 			console.log(message);
 		});
+		//웹소켓 연결 중 문제 발생 시 발생
 		ws.on("error", (error) => {
 			console.log(error);
 		});
+		//클라이언트와 연결이 끊겼을 때 발생
 		ws.on("close", () => {
 			console.log("클라이언트 접속 해제", ip);
+			//setInterval로 계속해서 서버에 메세지를 보내는 행위를 멈춤
+			//메모리 누수 방지
 			clearInterval(ws.interval);
 		});
+		//3초마다 연결 된 모든 클라이언에게 메세지를 보냄
 		const interval = setInterval(() => {
+			//readyState가 open인지 확인 후 (CONNECTING, OPEN, CLOSING, CLOSED 중 1개의 상태 )
 			if (ws.readyState === ws.OPEN) {
 				ws.send("서버에 클라이언트로 메세지를 보냅니다.");
 			}
